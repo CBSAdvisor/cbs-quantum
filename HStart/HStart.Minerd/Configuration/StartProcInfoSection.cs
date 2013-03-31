6b5cf873e7824c0a4f05d833aa0c41711feb6d1b
CBSAdvisor <cbs.advisor@gmail.com>
2013-03-31 15:53:51 +0300

    Fix Config

--- HStart/HStart.Minerd/Configuration/StartProcInfoSection.cs
+++ HStart/HStart.Minerd/Configuration/StartProcInfoSection.cs
@@ -11,5 +11,71 @@ namespace HStart.Configuration
     {
         private static string sConfigurationSectionConst = "StartProcInfoConfiguration";
 
+        public static StartProcInfoSection CreateSection()
+        {
+            StartProcInfoSection spiSection = null;
+
+            try
+            {
+
+                // Create a custom configuration section.
+                spiSection = new StartProcInfoSection();
+
+                // Get the current configuration file.
+                System.Configuration.Configuration config =
+                        ConfigurationManager.OpenExeConfiguration(
+                        ConfigurationUserLevel.None);
+
+                // Add the custom section to the application 
+                // configuration file. 
+                if (config.Sections[StartProcInfoSection.sConfigurationSectionConst] == null)
+                {
+                    config.Sections.Add(StartProcInfoSection.sConfigurationSectionConst, spiSection);
+                }
+
+
+                // Save the application configuration file.
+                spiSection.SectionInformation.ForceSave = true;
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+            catch (ConfigurationErrorsException)
+            {
+            }
+
+            return spiSection;
+        }
+
+        public static StartProcInfoSection GetConfig()
+        {
+            StartProcInfoSection spiSection = null;
+
+            try
+            {
+                // Get the application configuration file.
+                System.Configuration.Configuration config =
+                        ConfigurationManager.OpenExeConfiguration(
+                        ConfigurationUserLevel.None) as System.Configuration.Configuration;
+
+                // Read and display the custom section.
+                spiSection = ConfigurationManager.GetSection(StartProcInfoSection.sConfigurationSectionConst) as StartProcInfoSection;
+
+                if (spiSection == null)
+                {
+                    spiSection = CreateSection();
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+            }
+
+            return spiSection;
+        }
+
+        [ConfigurationProperty("StartProcInfos", IsDefaultCollection = false),
+        ConfigurationCollection(typeof(StartProcInfoElementCollection), AddItemName = "addProcInfo", ClearItemsName = "clearProcInfo", RemoveItemName = "removeProcInfo")]
+        public StartProcInfoElementCollection StartProcInfos
+        {
+            get { return this["StartProcInfos"] as StartProcInfoElementCollection; }
+        }
     }
 }
